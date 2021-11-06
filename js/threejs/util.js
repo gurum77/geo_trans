1616d194181a3f79fc88901d03617697f9ad4069
gurum77 <hileejaeho@gmail.com>
2021-11-06 22:37:49 +0900

    브이월드에 box 얹음.

---
+++ js/threejs/util.js
@@ -0,0 +1,7 @@
+function makeDefaultGeometry(scene) {
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshBasicMaterial({color:0x00ff00});
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+}
+
